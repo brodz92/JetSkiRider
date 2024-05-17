cfc91389fa8cdf2ab77c21a78046b1644a4807b3
brodz92 <163666491+brodz92@users.noreply.github.com>
2024-05-18 00:37:44 +1000

    update

--- Scripts/JetSki.gd
+++ Scripts/JetSki.gd
@@ -1,17 +1,20 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 150.0
 @onready var sprite = $Sprite2D
+var xDirection
+var yDirection
 
 
 
 func _physics_process(delta):
 	handleMovement()
+	handleAnimation()
 
 func handleMovement():
-	var xDirection = Input.get_axis("Left", "Right")
-	var yDirection = Input.get_axis("Up", "Down")
+	xDirection = Input.get_axis("Left", "Right")
+	yDirection = Input.get_axis("Up", "Down")
 	if xDirection or yDirection:
 		velocity.x = xDirection * SPEED
 		velocity.y = yDirection * SPEED
@@ -21,4 +24,11 @@ func handleMovement():
 	move_and_slide()
 
 func handleAnimation():
-	pass
+	if xDirection > 0:
+		sprite.rotation_degrees = 90
+	elif xDirection < 0:
+		sprite.rotation_degrees = -90
+	elif yDirection > 0:
+		sprite.rotation_degrees = 180
+	elif yDirection < 0:
+		sprite.rotation_degrees = 0
